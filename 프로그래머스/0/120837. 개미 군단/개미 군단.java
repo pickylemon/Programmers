d8fd4ed43b5ff2b8a97991e7a09ddbf41792b837
pickylemon <156510800+pickylemon@users.noreply.github.com>
2024-04-28 00:03:07 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(int hp) {
-        int top = hp / 5;
-        int middle = (hp - top*5) / 3;
-        int low = hp - top*5 - middle*3;
-        
-        return top + middle + low;
+        return hp / 5 + hp % 5 / 3 + hp % 5 % 3;
     }
 }
