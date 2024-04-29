cec6962df8eba89f5d3e1a19dc47b45b8bb09186
pickylemon <156510800+pickylemon@users.noreply.github.com>
2024-04-29 11:13:12 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 90.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        for(int i : num_list){
+            answer[i%2]++;
+        }
+        
+        return answer;
+    }
+}
