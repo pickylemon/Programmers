08de8638efccc51ed40406194e1b971ed61e9a5d
pickylemon <156510800+pickylemon@users.noreply.github.com>
2024-05-02 15:15:39 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.39 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,12 @@
+import java.util.stream.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int len = numbers.length;
+        Arrays.sort(numbers);
+        
+        return numbers[len-1]*numbers[len-2];
+        
+    }
+}
