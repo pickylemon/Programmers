17ca84b45fdb87955b6d6b6d50f0f9b9211be38c
pickylemon <156510800+pickylemon@users.noreply.github.com>
2024-05-04 16:07:37 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,15 +1,9 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        StringBuffer sb = new StringBuffer();
-        for(int i=0; i<my_string.length(); i++){
-            if(i == num1) {
-                sb.append(my_string.charAt(num2));
-            } else if (i == num2){
-                sb.append(my_string.charAt(num1));
-            } else {
-                sb.append(my_string.charAt(i));
-            }
-        }
-        return sb.toString();
+        char[] charArr = my_string.toCharArray();
+        charArr[num1] = my_string.charAt(num2);
+        charArr[num2] = my_string.charAt(num1);
+        
+        return String.valueOf(charArr);
     }
 }
