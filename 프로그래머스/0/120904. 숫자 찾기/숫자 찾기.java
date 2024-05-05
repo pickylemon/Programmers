fbafefef640caeb118720aa678d855e9e1840348
pickylemon <156510800+pickylemon@users.noreply.github.com>
2024-05-05 13:01:28 +0900

    [level 0] Title: 숫자 찾기, Time: 6.38 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        String[] numArr = ("" + num).split("");
-        String str = "" + k;
-        for(int i=0; i<numArr.length; i++) {
-            if(numArr[i].equals(str)){
-                return i+1;
-            }
-        }
-        return -1;
+        return (" " + num).indexOf(k+"");
     }
 }
