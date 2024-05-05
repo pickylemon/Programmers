accdc1218ecbd64404adbd657ef4c11912617f53
pickylemon <156510800+pickylemon@users.noreply.github.com>
2024-05-05 12:44:56 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 6.10 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -1,19 +1,11 @@
-import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(String my_string) {
-        List<Integer> list = new ArrayList<>();
-        for(char c : my_string.toCharArray()){
-            if('0'<= c && c <='9'){
-                list.add(c-'0');
-            }
-        }
-        Collections.sort(list);
-        int[] answer = new int[list.size()];
-        for(int i=0; i<answer.length; i++) {
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+        String[] tmp = my_string.replaceAll("[a-z|A-Z]","").split("");
+        return Stream.of(tmp)
+            .sorted()
+            .mapToInt(Integer::parseInt)
+            .toArray();
     }
 }
