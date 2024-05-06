8e18c3043ce9d84f537095b5af96433de37f0157
pickylemon <156510800+pickylemon@users.noreply.github.com>
2024-05-06 16:43:34 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,18 +1,12 @@
+import java.util.stream.*;
+
 class Solution {
     public String solution(String my_string) {
-        String[] strArr = my_string.split("");
-        StringBuffer sb = new StringBuffer();
-        
-        for(int i = 0; i < strArr.length; i++){
-            boolean duplicated = false;
-            for(int j = 0; j < i; j++){
-                if(strArr[i].equals(strArr[j])){
-                    duplicated = true;
-                    break;
-                }
-            }
-            if(!duplicated) {
-                sb.append(strArr[i]);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i < my_string.length(); i++){
+            char c = my_string.charAt(i);
+            if(my_string.indexOf(c) == i){
+                sb.append(c);
             }
         }
         
