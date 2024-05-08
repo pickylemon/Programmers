f16ca90352b675fe118c661b02c39b809bd6cdd7
pickylemon <156510800+pickylemon@users.noreply.github.com>
2024-05-08 17:33:28 +0900

    [level 0] Title: k의 개수, Time: 2.20 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,16 +1,13 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        StringBuffer sb = new StringBuffer();
-        for(int s=i; s<=j; s++){
-            sb.append(s);
+        int cnt = 0;
+        for(int s=i; s<=j; s++) {
+            int tmp = s;
+            while (tmp > 0) {
+                if(tmp % 10 == k) cnt++;
+                tmp /= 10;
+            }
         }
-        String tmp = sb.toString();
-        int beforeLen = tmp.length();
-        
-        String str = String.valueOf(k);
-        tmp = tmp.replace(str, "");
-        int afterLen = tmp.length();
-        
-        return beforeLen - afterLen;
+        return cnt;
     }
 }
