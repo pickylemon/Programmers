0b771976d292a0c9eab8886e9ce50d1deb78c8bb
pickylemon <156510800+pickylemon@users.noreply.github.com>
2024-05-11 20:07:12 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,18 +1,14 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        int cnt = 0;
-        boolean isAMember = false;
-        for(String[] data : db ){
-            cnt = 0; //매 라운드마다 0으로 초기화
+        String msg = "fail";
+        for(String[] data : db) {
             if(data[0].equals(id_pw[0])){
-                cnt++;
-                isAMember = true;
+                msg = "wrong pw";
                 if(data[1].equals(id_pw[1])){
-                    cnt++;
+                    msg = "login";
                 }
             } 
-            if(cnt == 2) return "login";
         }
-        return isAMember? "wrong pw" : "fail";
+        return msg;
     }
 }
