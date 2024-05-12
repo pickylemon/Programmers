75c3191358d8664a39ffaeac16b76859e7981125
pickylemon <156510800+pickylemon@users.noreply.github.com>
2024-05-12 20:51:50 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(int M, int N) {
-
-        int max = Math.max(M,N);
-        int min = Math.min(M,N);
-        
-        return (max-1) + (min-1) * max;
+        return M * N - 1;
     }
 }
