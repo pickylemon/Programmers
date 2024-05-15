876c76abb0d0264f13a22c2e16b8427592c75672
pickylemon <156510800+pickylemon@users.noreply.github.com>
2024-05-15 14:28:24 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,18 +1,14 @@
 class Solution {
     public int solution(int[] common) {
+        //등차이면서 등비일수는 없으니, 
+        //상대적으로 간단한 등차조건만 비교하고 나머지는 모두 등비 취급
+        
         int len = common.length;
         int cDiff = common[1] - common[0];
         
-        //만약 원소 중 0이 포함되어 있다면
-        for(int i : common) {
-            if(i == 0) return common[len-1] + cDiff;
-        }
-        //0이 포함되어 있지 않으면 등차, 등비 구분 필요
-        double cRatio = common[1] / (double)common[0];        
-        if(common[2]/(double)common[1] == cRatio) 
-            return (int)(common[len-1] * cRatio);
-        else {
+        if(common[2]-common[1] == cDiff)
             return common[len-1] + cDiff;
-        }
+        else return common[len-1] * (common[1]/common[0]);
+        
     }
 }
