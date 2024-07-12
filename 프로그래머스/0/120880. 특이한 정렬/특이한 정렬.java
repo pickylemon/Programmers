f8198d74744ebec5958f1f73e6e586512129b47d
pickylemon <156510800+pickylemon@users.noreply.github.com>
2024-07-12 15:06:24 +0900

    [level 0] Title: 특이한 정렬, Time: 3.64 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -2,21 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numlist, int n) {
-        for(int i=0; i<numlist.length-1; i++) {
-            for(int j=i+1; j<numlist.length; j++) {
-                if(Math.abs(n-numlist[i]) > Math.abs(n-numlist[j])) {
-                    int tmp = numlist[i];
-                    numlist[i] = numlist[j];
-                    numlist[j] = tmp;
-                } else if(Math.abs(n-numlist[i]) == Math.abs(n-numlist[j])
-                          && numlist[i] < numlist[j]) {
-                    int tmp = numlist[i];
-                    numlist[i] = numlist[j];
-                    numlist[j] = tmp;
-                }
-            }
-        }
-        
-        return numlist;
+        return Arrays.stream(numlist)
+                .boxed()
+                .sorted((a,b) -> Math.abs(a-n) == Math.abs(b-n) ?
+                                Integer.compare(b,a) : Integer.compare(
+                                                    Math.abs(a-n), 
+                                                    Math.abs(b-n)))
+                .mapToInt(Integer::intValue)
+                .toArray();
+
     }
 }
